9cced2362cf4495d76ec377da91b6bf3e70be71f
jiyinyiyong <jiyinyiyong@gmail.com>
2016-11-07 17:30:10 +0800

    got a demo and a README

--- README.md
+++ README.md
@@ -1,41 +1,58 @@
 
-Stack Workflow
+Debounced input demo
 ----
 
-Personal project template based on Respo, Boot, ClojureScript, Cirru Sepal...
+A demo in `core.async` of debounced input in a Respo component.
 
-### Develop
+Demo http://repo.respo.site/debounced-input-demo
 
-Genetate HTML(`target/index.html`), watch and build ClojureScript:
+This is the code briefly showing how it works:
 
-```bash
-boot dev!
-```
+```clojure
+; initial states and update function, notice `chan` is nil at first
+(defn init-state [& args] {:chan nil, :text ""})
+(defn update-state [state new-state] (merge state new-state))
 
-Compile and optimize ClojureScript, generate HTML with revision:
+(def time-gap 500)
 
-```bash
-boot build-advanced
-export boot_deps=`boot show -c`
-planck -c $boot_deps:src/ -i render.cljs
-```
+; this function is called by `on-input` to create the channel
+(defn setup-chan! [text mutate!]
+  ; initial channel created
+  (let [the-chan (chan)
+        timeout-ch (timeout time-gap)]
+    ; Respo function to set component state, this is a mutation
+    (mutate! {:chan the-chan})
+    ; anonymous recursion, see `recur` below
+    (go-loop
+     ; pass the text and timeout channel as parameters
+     [current-text text time-ch timeout-ch]
+     ; now pick data from channels and check
+     (let [[v c] (alts! [the-chan time-ch])]
+       (if (= c time-ch)
+         (do
+          ; so timeout now, get rid of the channel, set text
+          (close! the-chan)
+          (mutate! {:chan nil, :text current-text}))
+         (if (nil? v)
+          ; a closing channel may send nil, but doesn't matter here
+          nil
+          ; if typing it fast, continue with the recusion
+          (if (= v current-text)
+              (recur v time-ch)
+              (recur v (timeout time-gap)))))))
+    ; pass it to `on-input`
+    the-chan))
 
-Package jar file and install locally:
-
-```bash
-boot build
-```
-
-Package jar file and send to Clojars:
-
-```bash
-boot deploy
-```
-
-Get ClojureScript code:
-
-```bash
-boot generate-code
+(defn on-input [state mutate!]
+  (fn [e dispatch!]
+    (let [text (:text state)
+          input-chan (:chan state)
+          ; by default, there is no channel, create one
+          the-chan (if (some? input-chan)
+                       input-chan
+                       (setup-chan! text mutate!))]
+      ; and put `input.value` into the channel
+      (go (>! the-chan (:value e))))))
 ```
 
 ### Develop
--- src/debounced_input_demo/comp/widget.cljs
+++ src/debounced_input_demo/comp/widget.cljs
@@ -1,18 +1,66 @@
 
 (ns debounced-input-demo.comp.widget
-  (:require [respo.alias :refer [create-comp div input span]]
+  (:require [respo.alias :refer [create-comp div input span a]]
             [respo.comp.text :refer [comp-text]]
             [respo.comp.space :refer [comp-space]]
             [respo-ui.style :as ui]
-            [cljs.core.async :refer [chan <! >! timeout]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [cljs.core.async :refer [chan <! >! timeout close! alts!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(defn update-state [state] state)
+(def time-gap 500)
 
-(defn init-state [& args] nil)
+(defn setup-chan! [text mutate!]
+  (let [the-chan (chan), timeout-ch (timeout time-gap)]
+    (println "created chan.")
+    (mutate! {:chan the-chan})
+    (go-loop
+     [current-text text time-ch timeout-ch]
+     (println "waiting for a value")
+     (let [[v c] (alts! [the-chan time-ch])]
+       (if (= c time-ch)
+         (do
+          (println "timeout, destrying all")
+          (close! the-chan)
+          (mutate! {:chan nil, :text current-text}))
+         (if (nil? v)
+           (do (println "closed, nil") nil)
+           (do
+            (println "new value" (pr-str v) (pr-str current-text))
+            (if (= v current-text) (recur v time-ch) (recur v (timeout time-gap))))))))
+    the-chan))
+
+(defn on-input [state mutate!]
+  (println "render event")
+  (fn [e dispatch!]
+    (let [text (:text state)
+          input-chan (:chan state)
+          the-chan (if (some? input-chan) input-chan (setup-chan! text mutate!))]
+      (go (println "putting new value" (pr-str (:value e))) (>! the-chan (:value e))))))
+
+(defn update-state [state new-state] (merge state new-state))
+
+(defn init-state [& args] {:chan nil, :text ""})
 
 (defn render []
   (fn [state mutate!]
-    (div {} (input {:style ui/input}) (comp-space 16 nil) (comp-text state nil))))
+    (div
+     {}
+     (div
+      {}
+      (comp-text "This is a demo of a debounced input box." nil)
+      (comp-space 8 nil)
+      (a
+       {:attrs {:inner-text "Find more on GitHub",
+                :target "_black",
+                :href "https://github.com/Respo/debounced-input-demo"}}))
+     (div
+      {}
+      (input
+       {:style (merge ui/input {:width 400}),
+        :event {:input (on-input state mutate!)},
+        :attrs {:placeholder "Type here..."}})
+      (comp-space 16 nil)
+      (if (some? (:chan state)) (comp-text "Typing. The gap is 500ms." nil)))
+     (div {} (comp-text (:text state) nil)))))
 
 (def comp-widget (create-comp :widget init-state update-state render))
